f9f8670aa420959d91c139df3d7ebf8f0d965120
tangle <tangle@qq.com>
2019-06-24 19:08:56 +0800

    把本地提交

---
+++ git-project/src/com/itheima/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+        System.out.println("hello git pro");
+    }
+}
